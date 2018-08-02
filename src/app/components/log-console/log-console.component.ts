7e760f47c8c331e6449e6e39fd31c87605bf23f4
BUHOT Antoine <antoine.buhot@fime.com>
2018-08-02 09:08:16 +0200

    LogConsole : Style + Display on header click

--- src/app/components/log-console/log-console.component.ts
+++ src/app/components/log-console/log-console.component.ts
@@ -3,15 +3,21 @@ import { Component, OnInit } from '@angular/core';
 import { LoggingService } from '../../services/logging.service';
 
 @Component({
-  selector: 'app-log-console',
-  templateUrl: './log-console.component.html',
-  styleUrls: ['./log-console.component.css']
+	selector: 'app-log-console',
+	templateUrl: './log-console.component.html',
+	styleUrls: ['./log-console.component.css']
 })
 export class LogConsoleComponent implements OnInit {
-
-  constructor(private loggingService: LoggingService) { }
-
-  ngOnInit() {
-  }
-
+	
+	contentHidden: Boolean = true;
+	
+	constructor(private loggingService: LoggingService) { }
+	
+	ngOnInit() {
+	}
+	
+	display(): void {
+		this.contentHidden = !this.contentHidden;
+		console.log("click "+this.contentHidden);
+	} 
 }
