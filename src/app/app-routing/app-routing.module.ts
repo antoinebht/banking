5add268a2fb12087621de26f5abd513b7119addc
AntoineB <buhot.a@gmail.com>
2018-08-05 22:10:23 +0800

    Router : route to account

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AccountComponent } from '../components/account/account.component';
 
 const routes: Routes = [
-  
+  { path: 'account/:id', component: AccountComponent}
 ];
 
 @NgModule({
