fd0720c8da6e845ca145e2734b6d7399fa025044
BUHOT Antoine <antoine.buhot@fime.com>
2018-08-09 10:18:49 +0200

    AccountComponent : List the whole periods component

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AccountService } from '../../services/account.service';
+import { BankAccount } from '../../model/bank-account';
+
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+  account: BankAccount;
+
+  constructor(private accountService: AccountService) { 
+
+  }
 
   ngOnInit() {
+    this.accountService.getAccount(1).subscribe(account => {
+      this.account = account
+      console.log(account);
+    } );
   }
 
 }
