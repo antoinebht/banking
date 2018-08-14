867d5fe8ab99740e2b8aca58a087a03e023412b8
BUHOT Antoine <antoine.buhot@fime.com>
2018-08-14 04:54:55 +0200

    Header : Use bootstrap navbar + accounts list

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 import { AppConfigService } from '../../services/app-config.service';
+import { AccountService } from '../../services/account.service';
+
+import  { BankAccount } from '../../model/bank-account';
 
 @Component({
     selector: 'app-header',
@@ -10,14 +13,15 @@ import { AppConfigService } from '../../services/app-config.service';
 export class HeaderComponent implements OnInit {
     
     title: string;
-
     version: string;
+    accounts: BankAccount[];
 
-    constructor(private appConfigService: AppConfigService) { }
+    constructor(private appConfigService: AppConfigService, private accountService: AccountService) { }
     
     ngOnInit() {
         this.appConfigService.getTitle().subscribe(title => this.title = title);
         this.appConfigService.getVersion().subscribe(version => this.version = version);
+        this.accountService.getAccounts().subscribe(accounts => this.accounts = accounts);
     }
     
 }
