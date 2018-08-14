b561d9752517a94c609e9ed038748ef7c7c44ace
BUHOT Antoine <antoine.buhot@fime.com>
2018-08-14 10:08:39 +0200

    AccountPeriodComponent : Delete Operation

--- src/app/components/account-period/account-period.component.ts
+++ src/app/components/account-period/account-period.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 import { AccountPeriod } from '../../model/account-period';
+import { AccountOperation } from '../../model/account-operation';
 
 @Component({
   selector: 'app-account-period',
@@ -22,6 +23,15 @@ export class AccountPeriodComponent implements OnInit {
   addOperation(): void {
     this.period.operations.unshift({ date: '2018-06-31', amount: -6.0, type: 'COURSES', comment:'', checked: false })
   }
+
+  deleteOperation(operation: AccountOperation): void {
+    console.log("test");
+    const index: number = this.period.operations.indexOf(operation);
+    if (index !== -1) {
+      this.period.operations.splice(index, 1);
+    }  
+  }
+
   toogleCollapse(): void {
     this.collapse = !this.collapse;
   }
