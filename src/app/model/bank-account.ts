b85b04046e60573fef2d0ae48addcee1dcd9da53
BUHOT Antoine <antoine.buhot@fime.com>
2018-09-10 11:21:10 +0200

    Remove tags from accounts

--- src/app/model/bank-account.ts
+++ src/app/model/bank-account.ts
@@ -5,7 +5,6 @@ export class BankAccount {
     id: number = -1;
     name: string = "undefined";
     periods: AccountPeriod[] = [];
-    tagColors: Object = {};
 }
 
 export class BankAccountItem {
